cc686b668c950e2da5c2b19c4788441f44a26675
rotfrontAkk <elizavetatrusova2@gmail.com>
2022-10-22 17:05:45 -0400

    first commit add

---
+++ site/js/mail.js
@@ -0,0 +1,12 @@
+const defaultSelect = () => {
+    const element = document.querySelector('.default');
+    const choices = new Choices(element,{
+        searchEnabled: folse,
+    });
+
+};
+defaultSelect()
+
+
+
+var valuesSlider = document.getElementById('slider-fit');
